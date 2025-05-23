04ed1eae7223394c98eda3b5a4ce96e283da3d4e
Arpita Paul <72724463+paularpi66@users.noreply.github.com>
2025-05-23 16:38:18 +0600

    user input

--- user input.go
+++ user input.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	name, _ := r.ReadString('\n')
+
+	return strings.TrimSpace(name), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	//fmt.Print("Create a new Bill: ")
+	//name, _ := reader.ReadString('\n')
+	//name = strings.Trimepace(name)
+
+	name, _ := getInput("Bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("create a new Bill - ", b.name)
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose option: (a - add iteams, s - save bill, t - add tip ", reader)
+	fmt.Println(opt)
+}
 func main() {
-
+	mybill := createBill()
+	promptOptions(mybill)
+	fmt.Println(mybill)
 }
